09533d298db159ab4cbffc7ab9b9178a155e13e8
Carson Meiling <carmc93@gmail.com>
2020-03-28 10:53:41 -0600

    semantic and navbar addded

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import { Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import ContactForm from './components/ContactForm';
 import NoMatch from './components/NoMatch';
+import NavBar from './components/NavBar';
+import { Container } from 'semantic-ui-react';
 
 
 
 
 const App = () => (
   <Fragment>
+    <NavBar />
+    <Container>
     <Switch>
       <Route exact path ='/' component={Home} />
       <Route exact path ='/contact' component={ContactForm} />
       <Route component={NoMatch} />
     </Switch>
+    </Container>
   </Fragment>
 )
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import { Menu } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => (
+  <Menu>
+    <Link to='/'>
+      <Menu.Item>
+        Home
+      </Menu.Item>
+    </Link>
+    <Link to='/about'>
+      <Menu.Item>
+        About Me
+      </Menu.Item>
+    </Link>
+    <Link to='/contact'>
+      <Menu.Item>
+        Contact
+      </Menu.Item>
+    </Link>
+  </Menu>
 
-  
+
 )
 
 export default NavBar;
